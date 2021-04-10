88b6cdd491e1dfa69ebbbaed5075b497785af956
Marco Gorelli <marcogorelli@protonmail.com>
2021-04-10 21:12:11 +0100

    fix collections

--- pandas_dev_flaker/_plugins_tree/collections_abc.py
+++ pandas_dev_flaker/_plugins_tree/collections_abc.py
@@ -3,7 +3,10 @@ from typing import Iterator, Tuple
 
 from pandas_dev_flaker._data_tree import State, register
 
-MSG = "PDF001 don't import from collections.abc"
+MSG = (
+    "PDF001 import from collections.abc "
+    "(use 'from collections import abc' instead)"
+)
 
 
 @register(ast.ImportFrom)
@@ -13,4 +16,14 @@ def visit_ImportFrom(
     parent: ast.AST,
 ) -> Iterator[Tuple[int, int, str]]:
     if node.module == "collections.abc":
+        yield node.lineno, node.col_offset, MSG
+
+
+@register(ast.Import)
+def visit_Import(
+    state: State,
+    node: ast.Import,
+    parent: ast.AST,
+) -> Iterator[Tuple[int, int, str]]:
+    if "collections.abc" in {name.name for name in node.names}:
         yield node.lineno, node.col_offset, MSG
