3115ea0cbab554df53ffcfa4ce1c92f65f21d86b
Marco Gorelli <marcogorelli@protonmail.com>
2021-04-11 20:56:36 +0100

    numpy random test

--- pandas_dev_flaker/_plugins_tree/numpy_random.py
+++ pandas_dev_flaker/_plugins_tree/numpy_random.py
@@ -3,7 +3,7 @@ from typing import Iterator, Tuple
 
 from pandas_dev_flaker._data_tree import State, register
 
-MSG = "PDF014 don't import numpy.random"
+MSG = "PDF020 found import from numpy.random"
 
 
 @register(ast.ImportFrom)
@@ -12,8 +12,41 @@ def visit_ImportFrom(
     node: ast.ImportFrom,
     parent: ast.AST,
 ) -> Iterator[Tuple[int, int, str]]:
-    if node.module == "numpy.random" or (
-        node.module == "numpy"
-        and "random" in {name.name for name in node.names}
+    if (
+        node.module is not None
+        and node.module.split(".")[:2] == ["numpy", "random"]
+        or (
+            node.module == "numpy"
+            and "random" in {name.name for name in node.names}
+        )
+    ):
+        yield node.lineno, node.col_offset, MSG
+
+
+@register(ast.Import)
+def visit_Import(
+    state: State,
+    node: ast.Import,
+    parent: ast.AST,
+) -> Iterator[Tuple[int, int, str]]:
+    if ["numpy", "random"] in [
+        name.name.split(".")[:2] for name in node.names
+    ]:
+        yield node.lineno, node.col_offset, MSG
+
+
+@register(ast.Attribute)
+def visit_Attribute(
+    state: State,
+    node: ast.Attribute,
+    parent: ast.AST,
+) -> Iterator[Tuple[int, int, str]]:
+    if (
+        node.attr.split(".")[0] == "random"
+        and isinstance(node.value, ast.Name)
+        and node.value.id in {"numpy", "np"}
+    ) or (
+        isinstance(node.value, ast.Name)
+        and node.value.id.split(".")[:2] == ["numpy", "random"]
     ):
         yield node.lineno, node.col_offset, MSG
---
+++ tests/numpy_random_test.py
@@ -0,0 +1,59 @@
+import ast
+import tokenize
+from io import StringIO
+
+import pytest
+
+from pandas_dev_flaker.__main__ import run
+
+
+def results(s):
+    return {
+        "{}:{}: {}".format(*r)
+        for r in run(
+            ast.parse(s),
+            list(tokenize.generate_tokens(StringIO(s).readline)),
+        )
+    }
+
+
+@pytest.mark.parametrize(
+    "source",
+    (
+        pytest.param(
+            "import foo\nfoo.random",
+            id="foo.random",
+        ),
+        pytest.param(
+            "from foo import random",
+            id="foo.random",
+        ),
+    ),
+)
+def test_noop(source):
+    assert not results(source)
+
+
+@pytest.mark.parametrize(
+    "source, expected",
+    (
+        pytest.param(
+            "from numpy import random",
+            "1:0: PDF020 found import from numpy.random",
+            id="os.remove call",
+        ),
+        pytest.param(
+            "import numpy.random",
+            "1:0: PDF020 found import from numpy.random",
+            id="pytest.xfail used",
+        ),
+        pytest.param(
+            "import numpy as np\nnp.random.randn(3)",
+            "2:0: PDF020 found import from numpy.random",
+            id="pytest.xfail used",
+        ),
+    ),
+)
+def test_violation(source, expected):
+    (result,) = results(source)
+    assert result == expected
