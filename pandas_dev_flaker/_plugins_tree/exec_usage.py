9dd14021bd9aa55c2663dd59dce4e1b2a14b7399
Marco Gorelli <marcogorelli@protonmail.com>
2021-04-11 21:11:24 +0100

    exec test

--- pandas_dev_flaker/_plugins_tree/exec_usage.py
+++ pandas_dev_flaker/_plugins_tree/exec_usage.py
@@ -3,7 +3,7 @@ from typing import Iterator, Tuple
 
 from pandas_dev_flaker._data_tree import State, register
 
-MSG = "PDF006 builtin exec used"
+MSG = "PDF021 builtin exec used"
 
 
 @register(ast.Call)
@@ -12,6 +12,5 @@ def visit_Call(
     node: ast.Call,
     parent: ast.AST,
 ) -> Iterator[Tuple[int, int, str]]:
-    if isinstance(node.func, ast.Name):
-        if node.func.id == "exec":
-            yield (node.lineno, node.col_offset, MSG)
+    if isinstance(node.func, ast.Name) and node.func.id == "exec":
+        yield (node.lineno, node.col_offset, MSG)
