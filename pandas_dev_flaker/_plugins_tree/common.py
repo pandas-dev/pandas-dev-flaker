ddd8240af7cd0f02b793932e1bf81ef529ae5ae9
Marco Gorelli <marcogorelli@protonmail.com>
2021-04-12 09:27:42 +0100

    fixup common test

--- pandas_dev_flaker/_plugins_tree/common.py
+++ pandas_dev_flaker/_plugins_tree/common.py
@@ -7,6 +7,16 @@ from pandas_dev_flaker._data_tree import State, register
 MSG = "PDF009 'common' imported from 'pandas.core' without 'comm' alias"
 
 
+@register(ast.Import)
+def visit_Import(
+    state: State,
+    node: ast.Import,
+    parent: ast.AST,
+) -> Iterator[Tuple[int, int, str]]:
+    if check_for_wrong_alias(node.names, "pandas.core.common", "comm"):
+        yield node.lineno, node.col_offset, MSG
+
+
 @register(ast.ImportFrom)
 def visit_ImportFrom(
     state: State,
